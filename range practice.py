10b830f0ddf7d025934ebe6b74b03d28869029dd
PRao16 <48734576+PRao16@users.noreply.github.com>
2023-07-11 10:06:16 -0500

    Add files via upload

---
+++ range practice.py
@@ -0,0 +1,18 @@
+# Write your code here :-)THE RANGE()FUNCTION
+for numbers in range(10):
+    print (numbers)
+#print number 1-10
+
+for numbers in range(1,11):
+    print(numbers)
+
+#printing the numbers 1 t0 1000
+for numbers in range(1,1001):
+    print(numbers)
+
+print()#seperator
+
+#Making a list of numbers from 1 to a million
+
+numbers = list(range(1,1000001))
+print(numbers)
